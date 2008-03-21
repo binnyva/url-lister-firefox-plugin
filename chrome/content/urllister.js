51383410d760430b1ceff9bee96fe647669e510e
Binny V A <binnyva@gmail.com>
2008-03-21 18:33:41 +0530

    Seems that the project is done - its functionally ready

--- chrome/content/urllister.js
+++ chrome/content/urllister.js
@@ -1,4 +1,5 @@
 var UrlLister = {
+	delimiter: "\n\r",
 	/**
 	 * Adds a menu item called 'URL Lister...' to the menu that appear when a tab is right clicked.
 	 */
@@ -31,12 +32,13 @@ var UrlLister = {
 	 */
 	pasteUrls: function() {
 		document.getElementById("url-list").value = Clipboard.get();
+		if(Preference.get("extensions.urllister.openOnPaste")) UrlLister.openUrls();
 	},
 	
 	/// Happens when the OK button of the tab is clicked. Opens up the URLs in the textarea - if they are not already open.
 	openUrls:function () {
 		openNewTabWith = window.arguments[1];
-		var urls = document.getElementById("url-list").value.split("\n");
+		var urls = document.getElementById("url-list").value.split(UrlLister.delimiter);
 		NEW_URL_LOOP:
 		for(var i=0; i<urls.length; i++) {
 			// See if the URL is already open - if so, don't open it again.
@@ -64,7 +66,18 @@ var UrlLister = {
 		}
 		window.tab_list = tab_list;
 		
-		document.getElementById("url-list").value = UrlLister.getPlainText();
+		var all_delimiters = {"windows":"\n\r", "linux":"\n", "mac":"\r"};
+		UrlLister.delimiter = all_delimiters[Preference.get("extensions.urllister.delimiter")];
+		
+		//Get the URL list in the format given as the defaultFormat is the options.
+		var default_format = Preference.get("extensions.urllister.defaultFormat");
+		document.getElementById("format-menu").selectedIndex = default_format;
+		
+		if(default_format == 1)			document.getElementById("url-list").value = UrlLister.getHtmlAnchors();
+		else if(default_format == 2)	document.getElementById("url-list").value = UrlLister.getLinkedList();
+		else							document.getElementById("url-list").value = UrlLister.getPlainText();
+		
+		if(Preference.get("extensions.urllister.autoCopy")) UrlLister.copyUrls();
 		
 		// Set up all the events
 		//Event lister for format menu
@@ -83,19 +96,17 @@ var UrlLister = {
 		else document.getElementById("url-list").value = UrlLister.getPlainText();
 	},
 	
-	/**
-	 * Converts the URL array to plain text - just urls seperated by a new line
-	 */
+	/// Converts the URL array to plain text - just urls seperated by a new line
 	getPlainText: function() {
-		return window.tab_list.map(function(ele){return ele.url;}).join("\n");
+		return window.tab_list.map(function(ele){return ele.url;}).join(UrlLister.delimiter);
 	},
 	/// Converts the URL array to Anchors - in this format : <a href="url">Title</a><br />
 	getHtmlAnchors: function() {
-		return window.tab_list.map(function(ele){return '<a href="'+ele.url+'">'+ele.title+'</a><br />';}).join("\n");
+		return window.tab_list.map(function(ele){return '<a href="'+ele.url+'">'+ele.title+'</a><br />';}).join(UrlLister.delimiter);
 	},
 	/// Returns a bunch of anchors formated as a unordered list.
 	getLinkedList: function() {
-		return "<ul>\n" + window.tab_list.map(function(ele){return '<li><a href="'+ele.url+'">'+ele.title+'</a></li>';}).join("\n") + "\n</ul>";
+		return "<ul>" + UrlLister.delimiter + window.tab_list.map(function(ele){return '<li><a href="'+ele.url+'">'+ele.title+'</a></li>';}).join(UrlLister.delimiter) + UrlLister.delimiter + "</ul>";
 	},
 	
 	/**
@@ -105,37 +116,6 @@ var UrlLister = {
 	 */
 	showMainDialog: function () {
 		window.openDialog("chrome://urllister/content/main_dialog.xul", "URL Lister", "chrome,width=600,height=320", gBrowser, openNewTabWith);
-	}
-}
-
-/**
- * The clipboard library - hopefully, from the next versino we will be able to use FUEL.
- */
-var Clipboard = {
-	"set": function(text) {
-		var clipboard = Components.classes['@mozilla.org/widget/clipboardhelper;1'].getService(Components.interfaces.nsIClipboardHelper);
-		clipboard.copyString(text);
-	},
-	
-	"get": function() {
-		// Yeah - its a bit weird
-		// http://www.xulplanet.com/tutorials/mozsdk/clipboard.php
-		var clipboard = Components.classes["@mozilla.org/widget/clipboard;1"].getService(Components.interfaces.nsIClipboard);
-		if (!clipboard) return false;
-		
-		var trans = Components.classes["@mozilla.org/widget/transferable;1"].createInstance(Components.interfaces.nsITransferable);
-		if(!trans) return false; 
-		trans.addDataFlavor("text/unicode");
-		clipboard.getData(trans,clipboard.kGlobalClipboard);
-		
-		var str = new Object();
-		var strLength = new Object();
-		trans.getTransferData("text/unicode",str,strLength);
-		var pastetext;
-		if (str) str = str.value.QueryInterface(Components.interfaces.nsISupportsString); 
-		if (str) pastetext = str.data.substring(0,strLength.value / 2);
-		
-		return pastetext;
 	}
 }
 
