50dc9bdf777110f2fbc0f4f6fe924a0510fff6fa
Binny V A <binnyva@gmail.com>
2008-03-22 22:58:38 +0530

    The default option values

---
+++ defaults/preferences/urllister.js
@@ -0,0 +1,6 @@
+pref("extensions.urllister.defaultFormat", 0);
+pref("extensions.urllister.autoCopy", false);
+pref("extensions.urllister.openOnPaste", false);
+pref("extensions.urllister.delimiter", "windows");
+
+//pref("extensions.urllister@urllister.net.description", "chrome://urllister/locale/urllister.properties");  
